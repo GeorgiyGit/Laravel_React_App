2c3bbbb4e49636ad486b2854fbcfdf0f7ac15bba
GeorgiyGit <102670724+GeorgiyGit@users.noreply.github.com>
2023-01-29 10:30:39 +0200

    add paggination and redux

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -1,44 +1,82 @@
-import { render } from "@testing-library/react";
 import { useEffect, useState } from "react";
-import http from "../../../http_common";
+import classNames from "classnames";
 import { ISimpleProduct } from "../models/simple-product";
 import Loader from "./Loader";
+import { useActions } from "../../../hooks/useActions";
+import { IProductSearch } from "../../../store/types/productTypes";
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [list, setList] = useState<Array<ISimpleProduct>>([]);
-  const[isLoading,setLoading]=useState<boolean>(true);
+  const { GetProductList } = useActions();
+  const [search, setSearch] = useState<IProductSearch>({
+    page: 1,
+  });
 
-  useEffect(()=> {//get products
-    http.get<Array<ISimpleProduct>>("api/products").then(async (res) => {
-      const { data } = res;
-      console.log(data);
-      setList(data);
-      console.log(list);
-      setLoading(false);
-    });
-  }, []);
+  const { list, current_page, count_page, total } = useTypedSelector(
+    (store) => store.product
+  );
+  // useSelector((store: any) => store.product as IProductState);
 
-  const content = list.map((product) => (//view products with cards
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{product.id}</h5>
-        <p className="card-text">{product.name}</p>
+  useEffect(() => {
+    GetProductList(search);
+    isLoading = false;
+  }, [search]);
+
+  //get products
+  //http.get<Array<ISimpleProduct>>("api/products").then(async (res) => {
+  //const { data } = res;
+  //console.log(data);
+  //setList(data);
+  //console.log(list);
+  //setLoading(false);
+  //});
+  //}, []);
+  const buttons: Array<number> = [];
+  for (let i = 1; i <= count_page; i++) buttons.push(i);
+
+  const paginate = buttons.map((page) => {
+    return (
+      <li key={page} className="page-item">
+        <Link
+          to={"?page=" + page}
+          onClick={() => setSearch({ page })}
+          className={classNames("page-link", { active: page === current_page })}
+        >
+          {page}
+        </Link>
+      </li>
+    );
+  });
+
+  const content = list.map(
+    (
+      product //view products with cards
+    ) => (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{product.id}</h5>
+          <p className="card-text">{product.name}</p>
+        </div>
       </div>
-    </div>
-  ));
+    )
+  );
 
   let loadDiv;
-  if(isLoading){
-    loadDiv=<Loader/>//loader
-  }
-  else{
-    loadDiv=null;
+  let isLoading = false;
+  if (isLoading) {
+    loadDiv = <Loader />; //loader
+  } else {
+    loadDiv = null;
   }
   return (
     <>
       <h1 className="text-center">Home Page</h1>
 
       <div className="containter">
+        <nav>
+          <ul className="pagination">{paginate}</ul>
+        </nav>
         {loadDiv}
         {content}
       </div>
---
+++ src/store/action-creators/index.ts
@@ -0,0 +1,6 @@
+import * as ProductActionCreators from '../../features/home/action'
+const actions = {
+    ...ProductActionCreators
+}
+
+export default actions;
